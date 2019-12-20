0f9a73f74feb66f1f23b2b74639ab057b774458e
Code4Blessings <robinwarden@code4blessings.com>
2019-12-20 11:34:59 -0500

    Built Private Route component and axiosWithAuth module

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,9 @@
-import React, { useState } from "react";
+import React, { BrowserRouter as Router} from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>, rootElement);
