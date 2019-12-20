1e399216c2c94f98e7b17ea2bb0aa4fbbfa8a139
Code4Blessings <robinwarden@code4blessings.com>
2019-12-20 12:58:30 -0500

    Built Login page and made a minor adjustment to the CSS

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [login, setLogin] = useState(
+    {
+      credentials: {
+         username: '',
+         password: ''
+      },
+      isLoading: false
+    }
+  )
+
+  const changeHandler = e => {
+    setLogin({
+      credentials: {
+        ...login.credentials,
+        [e.target.name]: e.target.value
+      }
+    })
+  }
+
+  const submitLogin = e => {
+    e.preventDefault();
+    setLogin({ isLoading: true })
+    axiosWithAuth()
+      .post('/', login.credentials)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload)
+        props.history.push('/protected')
+      })
+      .catch(err => console.log('Data returned an error', err))
+  }
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={submitLogin} className="form-container">
+        <label htmlFor="username">Username:
+           <input 
+              type="text"
+              name="username"
+              value={login.credentials.username} 
+              onChange={changeHandler} />
+        </label>
+        <label htmlFor="password">Password:
+           <input 
+              type="text"
+              name="password"
+              value={login.credentials.password} 
+              onChange={changeHandler} />
+        </label>
+      </form>
     </>
   );
 };
--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,5 @@
-import React, { BrowserRouter as Router} from "react";
+import React from "react";
+import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from "react-dom";
 import App from "./App";
 
