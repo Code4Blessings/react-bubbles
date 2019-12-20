1d3e87a76e498af832ca7f7452e5e92b3477f485
Code4Blessings <robinwarden@code4blessings.com>
2019-12-20 13:11:49 -0500

    Refactored Login page to class components

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,61 +1,67 @@
-import React, { useState } from "react";
-import { axiosWithAuth } from "../utils/axiosWithAuth";
+import React, { Component } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
-const Login = (props) => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  const [login, setLogin] = useState(
-    {
-      credentials: {
-         username: '',
-         password: ''
-      },
-      isLoading: false
+class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            credentials: {
+                username: '',
+                password: ''
+            },
+            isLoading: false
+        }
+    };
+
+    handleChange = e => {
+       this.setState({
+           credentials: {
+               ...this.state.credentials,
+            [e.target.name]: e.target.value   
+           }
+       })
     }
-  )
 
-  const changeHandler = e => {
-    setLogin({
-      credentials: {
-        ...login.credentials,
-        [e.target.name]: e.target.value
-      }
-    })
-  }
+    login = e => {
+        e.preventDefault();
+        this.setState({
+            isLoading: true
+        })
+        axiosWithAuth()
+        .post('/login', this.state.credentials)
+        .then(res => {
+            localStorage.setItem('token', res.data.payload);
+            this.props.history.push('/protected')
+        })
+        .catch(err => console.log('Data returned an error', err))
+    }
 
-  const submitLogin = e => {
-    e.preventDefault();
-    setLogin({ isLoading: true })
-    axiosWithAuth()
-      .post('/', login.credentials)
-      .then(res => {
-        localStorage.setItem('token', res.data.payload)
-        props.history.push('/protected')
-      })
-      .catch(err => console.log('Data returned an error', err))
-  }
+    render() {
+        return (
+            <div>
+              <h1>Welcome to the Bubble App!</h1>
+                <form onSubmit={this.login} className="form-container">
+                    <label htmlFor="username">Username:
+                        <input 
+                        type="text"
+                        name="username"
+                        value={this.state.credentials.username} 
+                        onChange={this.handleChange} />
+                    </label>
 
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <form onSubmit={submitLogin} className="form-container">
-        <label htmlFor="username">Username:
-           <input 
-              type="text"
-              name="username"
-              value={login.credentials.username} 
-              onChange={changeHandler} />
-        </label>
-        <label htmlFor="password">Password:
-           <input 
-              type="text"
-              name="password"
-              value={login.credentials.password} 
-              onChange={changeHandler} />
-        </label>
-      </form>
-    </>
-  );
-};
+                    <label htmlFor="password">Password:
+                        <input 
+                        type="text"
+                        name="password"
+                        value={this.state.credentials.password} 
+                        onChange={this.handleChange} />
+                    </label>
+                    <button>Login</button>
+                    {this.state.isLoading && 'logging in'}
+                </form>
+            </div>
+        );
+    }
+}
 
 export default Login;
